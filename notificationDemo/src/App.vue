2e89b9fe0a7675b4f26afbc7fe371cfe31c2a1d3
zhannan-cn <zhannan1997@yahoo.com>
2023-03-20 14:27:27 +0800

    Add files via upload

---
+++ notificationDemo/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { NButton, useNotification, NNotificationProvider, NConfigProvider } from 'naive-ui'
+import HelloWorld from './components/HelloWorld.vue';
+
+
+// const onButton = () => {
+//   notification.info({
+//     title: 'Info',
+//     content: 'This is a info message.'
+//   })
+// }
+
+</script>
+
+<template>
+  <n-config-provider>
+    <n-notification-provider>
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </n-notification-provider>
+  </n-config-provider>
+</template>
+
+<style scoped>
+</style>
---
+++ notificationDemo/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { NButton, useNotification } from 'naive-ui'
+defineProps<{ msg: string }>()
+const count = ref(0)
+const notification = useNotification()
+
+const onButton = () => {
+  const message = {
+    "statusCode": 400,
+    "data": null,
+    "succeeded": false,
+    "errors": "有数据没有处理完毕, 请在日志中查看! ,\r\n 日志路径: D:\\Code\\C#\\General\\General\\General.Web.Entry\\bin\\Debug\\net7.0\\log\\AgencyNameDots.txt",
+    "extras": null,
+    "timestamp": 1679293131
+  }
+  notification.warning({
+    title: '警告',
+    description: message.errors
+  })
+}
+</script>
+
+<template>
+  <n-button @click="onButton">Click Me</n-button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
